731f5c299746ac7b1604dbf293f4ade452921522
Son Vo <vnsonvo@gmail.com>
2022-07-15 22:21:31 +0700

    add middleware

--- server.js
+++ server.js
@@ -21,6 +21,20 @@ MongoClient.connect(dbConnectionString).then((client) => {
   collection = db.collection("movies");
 });
 
+// Middleware
+// set view engine - template engine
+app.set("view engine", "ejs");
+// serving static files in public folder
+app.use(express.static("public"));
+// URL-encoded data will be parsed
+app.use(express.urlencoded({ extended: true }));
+// parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
+/* express.json() will parse the body from post/fetch request except from html post form. It wont parse information from the html post form
+So express.json() is a body parser for post request except html post form and express.urlencoded({extended: true}) is a body parser for html post form */
+// implementing CORS helps to access numerous functionalities on the browser
+app.use(cors());
+
 app.listen(PORT, (_) => {
   console.log(`Server is running on port ${PORT}`);
 });
