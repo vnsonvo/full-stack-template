b0a7510c7812f7ef6d0f3fef4ab10bcc667ce4ee
Son Vo <vnsonvo@gmail.com>
2022-07-15 22:02:54 +0700

    listen to port and use nodemon for dev

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ let db,
   dbName = "sample_mflix",
   collection;
 
+// create PORT
 const PORT = process.env.PORT || 8000;
 
 // connect to Mongo
@@ -18,4 +19,8 @@ MongoClient.connect(dbConnectionString).then((client) => {
   console.log(`Connected to ${dbName} database.`);
   db = client.db(dbName);
   collection = db.collection("movies");
+});
+
+app.listen(PORT, (_) => {
+  console.log(`Server is running on port ${PORT}`);
 });
